d77a00f79fce06889797744547f6d04a34f9c2b6
VTG56 <vishwaradhya.aihole@gmail.com>
2025-08-23 23:23:12 +0530

    reduced particle count

--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -222,7 +222,7 @@ const upperStateCode = formData.stateCode.toUpperCase();
         color: { value: "#ffffff" },
         links: { color: "#ffffff", distance: 150, enable: true, opacity: 0.2, width: 1 },
         move: { direction: "none", enable: true, outModes: { default: "bounce" }, random: false, speed: 1.5, straight: false },
-        number: { density: { enable: true }, value: 150 },
+        number: { density: { enable: true }, value: 50 },
         opacity: { value: 0.5 },
         shape: { type: "circle" },
         size: { value: { min: 1, max: 7 } },
--- src/pages/ThankYouPage.js
+++ src/pages/ThankYouPage.js
@@ -59,7 +59,7 @@ function ThankYouPage() {
         color: { value: "#ffffff" },
         links: { color: "#ffffff", distance: 150, enable: true, opacity: 0.2, width: 1 },
         move: { direction: "none", enable: true, outModes: { default: "bounce" }, random: false, speed: 1.5, straight: false },
-        number: { density: { enable: true }, value: 150 },
+        number: { density: { enable: true }, value: 100 },
         opacity: { value: 0.5 },
         shape: { type: "circle" },
         size: { value: { min: 1, max: 7 } },
